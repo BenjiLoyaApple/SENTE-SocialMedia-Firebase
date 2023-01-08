c9969c30c325765581eb34ae6d93065d9a6e55c3
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2023-01-08 12:33:12 +0500

    Initial Commit

---
+++ UPapp-Firebase/UPapp_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UPapp_FirebaseApp.swift
+//  UPapp-Firebase
+//
+//  Created by Benji Loya on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct UPapp_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
